88018afe40ee19863a7772232f5de0b3700c7603
PubSec <pubsecs@proton.me>
2025-03-13 16:25:17 +0300

    added some modifications

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,31 @@ use::clap::Parser;
 
 fn main() {
 
-    let args = Args::parse();
+    let  args = Args::parse();
     for _ in 0..args.count{
-        Command::new(&args.command_name).output().expect("An error");
+        let _ = Command::new(&args.command_name).spawn();
+
+
+    if args.shutdown == 'y'{
+        println!("Command finished shutdowning down now");
+        let _output = Command::new("shutdown").output();
+
+        }
     }
 }
 
 #[derive(Parser,Debug)]
 #[command(version = "1.0.0",about = "The program allows you to repeat a command",long_about = None)]
 struct Args{
-    #[arg(short='n',long="command-name")]
+
+    /// Command name eg ls,cat...
     command_name:String,
 
+    /// Number of times the command will be executed
     #[arg(short,long, default_value_t = 1)]
     count:u8,
 
+    /// Shutdown computer after command is executed
     #[arg(short,long,default_value_t = 'n')]
     shutdown: char,
 }
